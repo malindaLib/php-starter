f61c58104063986f757fd87f65f1368af06a5aa2
NEroousl <s92083180@ousl.lk>
2026-08-22 01:18:07 +0530

    message

--- app/index.php
+++ app/index.php
@@ -1,11 +1,26 @@
 <?php
-// Enter your code here, enjoy!
-$array = array("1" => "PHP code tester Sandbox Online (v4)",
-    "emoji" => "😀 😃 😄 😁 😆", 5 , 5 => 89009,
-    "Random number" => rand(100,999),
-    "PHP Version" => phpversion()
-);
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-foreach( $array as $key => $value ){
-    echo $key."\t=>\t".$value."\n";
-}
+/**
+ * Throw an exception when users try to access index.php outside of the web root.
+ *
+ * @package    core
+ * @copyright  Andrew Lyons <andrew@nicols.co.uk>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+require_once('config.php');
+
+throw new \core\exception\moodle_exception('rootdirpublic', 'error');
